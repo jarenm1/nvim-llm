69b00119c1086411c9f373f9581adede63c48b2c
jarenm1 <jarenmchugh@gmail.com>
2025-03-16 19:37:13 -0500

    changes

--- lua/nvim-llm-chat/init.lua
+++ lua/nvim-llm-chat/init.lua
@@ -3,10 +3,10 @@ local M = {}
 function M.setup()
   vim.api.nvim_create_user_command("Chat", function()
     require("nvim-llm-chat.chat").toggle_chat()
-  end, { desc = "Toggle Nvim-LLM chat window" })
+  end, { desc = "Toggle Nvim-LLM Chat window" })
 
-  -- Optional keymap for LazyVim style
-  vim.keymap.set("n", "<leader>cc", ":Chat<CR>", { desc = "Open Nvim-LLM Chat" })
+  -- Optional keymap for Nvim-LLM style
+  vim.keymap.set("n", "<leader>aa", ":Chat<CR>", { desc = "Open Nvim-LLM Chat" })
 end
 
 return M
